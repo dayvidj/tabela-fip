7264c7b20b564a5bc9e1e5d455a387e0db8a95fc
Dayvid Jonathan <dayvidaf7@gmail.com>
2023-12-11 20:16:57 -0300

    Update menu classe Principal

--- src/main/java/br/com/alura/TabelaFipe/model/ModeloVeiculo.java
+++ src/main/java/br/com/alura/TabelaFipe/model/ModeloVeiculo.java
@@ -2,8 +2,9 @@ package br.com.alura.TabelaFipe.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record ModeloVeiculo (List<DadosMarca> modelos){
+public record ModeloVeiculo (@JsonAlias("modelos") List<DadosMarca> listaModelos){
 }
--- src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
@@ -15,48 +15,68 @@ public class Principal {
 	private Scanner sc = new Scanner(System.in);
 	private ConsumoApi consumo = new ConsumoApi();
 	private ConverteDados conversor = new ConverteDados();
-	
+
 	private final String URL_API = "https://parallelum.com.br/fipe/api/v1/";
 
 	public void exibeMenu() {
-
-		System.out.println("INFORME O TIPO DE VEÍCULO PARA CONSULTA: \n-CARROS \n-MOTOS \n-CAMINHOES");
+		System.out.println("INFORME O TIPO DE VEÍCULO PARA CONSULTA:\n-CARROS\n-MOTOS\n-CAMINHOES");
 		var tipo = sc.nextLine().toLowerCase();
+
+		List<DadosMarca> marcas = obterMarcas(tipo);
+		marcas.forEach(System.out::println);
+
+		System.out.print("\nDigite o código da marca: ");
+		var codigoMarca = sc.nextLine();
+
+		ModeloVeiculo modelos = obterModelos(tipo, codigoMarca);
+		modelos.listaModelos().forEach(System.out::println);
+
+		System.out.print("\nDigite um trecho do nome do modelo (Ex.: Hilux): ");
+		var nomeModelo = sc.nextLine().toUpperCase();
+		filtrarModeloPorNome(nomeModelo, modelos);
+
+		System.out.print("\nDigite o código do modelo a ser consultado: ");
+		var codigoModelo = sc.nextLine();
+
+		List<DadosVeiculo> listaVeiculos = obterVeiculos(tipo, codigoMarca, codigoModelo);
+		System.out.println("\nTodos os veículo filtrados com avaliações por ano:");
+		listaVeiculos.forEach(System.out::println);
+	}
+
+	private List<DadosMarca> obterMarcas(String tipo) {
 		String endereco = URL_API + tipo + "/marcas";
 		var json = consumo.obterDados(endereco);
 //		System.out.println(json);	
 		var dadosMarca = conversor.obterLista(json, DadosMarca.class);
-		dadosMarca.forEach(System.out::println);
+		return dadosMarca;
+	}
 
-		System.out.print("\nDigite o código da marca: ");
-		var codigoMarca = sc.nextLine();
-		endereco += "/" + codigoMarca + "/modelos";
-		
-		json = consumo.obterDados(endereco);
-		var listaMarca = conversor.obterDados(json, ModeloVeiculo.class);
-		listaMarca.modelos().forEach(System.out::println);
-		
-		System.out.print("\nDigite um trecho do nome do modelo (Ex.: Hilux): ");
-		var nomeModelo = sc.nextLine().toUpperCase();
-		List<DadosMarca> listaModelo = listaMarca.modelos().stream().filter(m -> m.nome().toUpperCase().contains(nomeModelo)).collect(Collectors.toList());
-		listaModelo.forEach(System.out::println);
-		
-		System.out.print("\nDigite o código do modelo a ser consultado: ");
-		var codigoModelo = sc.nextLine();
-		endereco += "/" + codigoModelo + "/anos";
-		json = consumo.obterDados(endereco);
-		
-		var modeloAno = conversor.obterLista(json, DadosMarca.class);
+	private ModeloVeiculo obterModelos(String tipo, String codigoMarca) {
+		String endereco = URL_API + tipo + "/marcas/" + codigoMarca + "/modelos";
+		var json = consumo.obterDados(endereco);
+		var marcaModelos = conversor.obterDados(json, ModeloVeiculo.class);
+		return marcaModelos;
+	}
+
+	private void filtrarModeloPorNome(String nomeModelo, ModeloVeiculo modelo) {
+		List<DadosMarca> listaModelos = modelo.listaModelos().stream()
+				.filter(m -> m.nome().toUpperCase().contains(nomeModelo)).collect(Collectors.toList());
+		listaModelos.forEach(System.out::println);
+	}
+
+	private List<DadosVeiculo> obterVeiculos(String tipo, String codigoMarca, String codigoModelo) {
+		String endereco = URL_API + tipo + "/marcas/" + codigoMarca + "/modelos/" + codigoModelo + "/anos";
+		var json = consumo.obterDados(endereco);
+
+		List<DadosMarca> modeloAno = conversor.obterLista(json, DadosMarca.class);
 //		modeloAno.forEach(System.out::println);
-		
+
 		List<DadosVeiculo> veiculos = new ArrayList<>();
-		for(DadosMarca d: modeloAno) {
-			json = consumo.obterDados(endereco + "/"+ d.codigo());
+		for (DadosMarca d : modeloAno) {
+			json = consumo.obterDados(endereco + "/" + d.codigo());
 			veiculos.add(conversor.obterDados(json, DadosVeiculo.class));
 		}
-		System.out.println("\nTodos os veículo filtrados com avaliações por ano:");
-		veiculos.forEach(System.out::println);
-		
+		return veiculos;
 	}
 
 }
